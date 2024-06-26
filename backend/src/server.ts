a384c9712002ddc6583f6a350b65547d6b8b2624
shubham-gund <shubhamgund91@gmail.com>
2024-06-26 15:56:20 +0530

    add user routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js"
-import connectMongoDB from "./db/connection.js";
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser"
+import {v2 as cloudinary} from "cloudinary"
+
+import authRoutes from "./routes/auth.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
+import connectMongoDB from "./db/connection.js";
+
 dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 const app = express();
 
@@ -11,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
 
 app.get("/",(req,res)=>{
   res.send("Hello World");
