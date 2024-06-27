4dfec27cecc51eb8669b98afcf8708c1a47bbc6f
shubham-gund <shubhamgund91@gmail.com>
2024-06-27 14:39:55 +0530

    add Post routes

---
+++ backend/src/routes/post.routes.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { protectedRoute } from "../middleware/protectedRoute.js";
+import { commentOnPost, createPost, deletePost, likeUnlikePost } from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+router.post("/create",protectedRoute,createPost)
+router.post("/like/:id",protectedRoute,likeUnlikePost)
+router.post("/comment/:id",protectedRoute,commentOnPost)
+router.delete("/:id",protectedRoute,deletePost)
+
+
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,8 @@ import {v2 as cloudinary} from "cloudinary"
 
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
-
+import postRoutes from "./routes/post.routes.js"
+ 
 import connectMongoDB from "./db/connection.js";
 
 dotenv.config();
@@ -20,8 +21,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
+
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
+app.use("/api/posts",postRoutes);
 
 app.get("/",(req,res)=>{
   res.send("Hello World");
