ade9f202f65e7e2f89c8a9dddf167cd256061749
shubham-gund <shubhamgund91@gmail.com>
2024-06-30 20:11:36 +0530

    add pages:notification/profile

--- frontend/src/components/common/Posts.tsx
+++ frontend/src/components/common/Posts.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Post from "./Post";
+import PostSkeleton from "../skeleton/PostSkeleton.js";
+import { POSTS } from "../../utils/db/dummy";
+import { PostType } from "../../types.js";
 
-function Posts() {
-  return (
-    <div>Posts</div>
-  )
-}
 
-export default Posts
+const Posts = () => {
+	const isLoading = false;
+
+	return (
+		<>
+			{isLoading && (
+				<div className='flex flex-col justify-center'>
+					<PostSkeleton />
+					<PostSkeleton />
+					<PostSkeleton />
+				</div>
+			)}
+			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+			{!isLoading && POSTS && (
+				<div>
+					{POSTS.map((post:PostType) => (
+						<Post key={post._id} post={post} />
+					))}
+				</div>
+			)}
+		</>
+	);
+};
+
+export default Posts;
---
+++ frontend/src/types.ts
@@ -0,0 +1,33 @@
+// types.ts
+export interface UserType {
+  username: string;
+  profileImg: string;
+  fullName: string;
+}
+
+export interface CommentType {
+  _id: string;
+  text: string;
+  user: UserType;
+}
+
+export interface PostType {
+  _id: string;
+  text: string;
+  img?: string;
+  user: UserType;
+  comments: CommentType[];
+  likes: string[];
+}
+
+export interface NotificationFromType {
+  _id: string;
+  username: string;
+  profileImg: string;
+}
+
+export interface NotificationType {
+  _id: string;
+  from: NotificationFromType;
+  type: "follow" | "like"; // Add other notification types as needed
+}
