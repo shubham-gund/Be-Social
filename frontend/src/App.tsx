97fd365d664a6fdf6bffb644c62bbb9493139baa
shubham-gund <shubhamgund91@gmail.com>
2024-07-07 16:30:22 +0530

    remove sidebar and suggestion section for login and signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -35,7 +35,7 @@ function App() {
 	return (
 		<div className='flex max-w-6xl mx-auto'>
       {/* common component */}
-      <Sidebar/>
+      {authUser && <Sidebar />}
 			<Routes>
 				<Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login"/>} />
 				<Route path='/login' element={!authUser ?<LoginPage />:<Navigate to="/"/>} />
@@ -43,7 +43,7 @@ function App() {
 				<Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to="/login"/>} />
 				<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to="/login"/>} />
 			</Routes>
-      <RightPanel/>
+      {authUser && <RightPanel />}
 			<Toaster/>
 		</div>
 	);
