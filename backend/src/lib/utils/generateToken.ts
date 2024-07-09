0f1a993d75abf6d43143abca45cbb15be1eacb9f
shubham-gund <shubhamgund91@gmail.com>
2024-07-09 17:05:25 +0530

    removed node env

--- backend/src/lib/utils/generateToken.ts
+++ backend/src/lib/utils/generateToken.ts
@@ -11,7 +11,6 @@ export const generateTokenAndSetCookie = (userId:string,res:Response)=>{
   res.cookie("jwt",token,{
     maxAge:15*24*60*60*1000,
     httpOnly:true,
-    sameSite:"strict",
-    secure:process.env.NODE_ENV !== "development",
+    sameSite:"strict"
   })
 }
