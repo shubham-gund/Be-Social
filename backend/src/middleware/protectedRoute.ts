ee849cde7fba02c1e876526c415edab06cbd0a50
shubham-gund <shubhamgund91@gmail.com>
2024-07-09 21:26:59 +0530

    changed format of token

--- backend/src/middleware/protectedRoute.ts
+++ backend/src/middleware/protectedRoute.ts
@@ -10,7 +10,7 @@ interface DecodedToken {
 export const protectedRoute = async (req: Request, res: Response, next: NextFunction) => {
   try {
     // const token = req.cookies.jwt_token;
-    const token = req.headers.authorization?.split(' ')[1];  // token
+    const token = req.headers.authorization;  // token
     console.log(token)
     if (!token) {
       return res.status(401).json({ message: "Unauthorized token not found" });
