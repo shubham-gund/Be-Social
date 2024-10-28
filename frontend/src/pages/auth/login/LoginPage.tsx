de8bec3dcf2e612548a79eabd321cca4cf5953d9
shubham-gund <shubhamgund91@gmail.com>
2024-10-28 22:33:32 +0530

    ui changes

--- frontend/src/pages/auth/login/LoginPage.tsx
+++ frontend/src/pages/auth/login/LoginPage.tsx
@@ -64,11 +64,11 @@ const LoginPage = () => {
 
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen'>
-			<div className='flex-1 hidden lg:flex items-center justify-center'>
-				<XSvg className='lg:w-2/3 fill-white' />
+			<div className='flex-1 hidden lg:flex items-center justify-center px-16'>
+				<XSvg className='lg:w-80 fill-white' />
 			</div>
 			<div className='flex-1 flex flex-col justify-center items-center'>
-				<form className='flex gap-4 flex-col' onSubmit={handleSubmit}>
+				<form className='lg:w-2/3  mx-auto md:mx-20 flex gap-4 flex-col' onSubmit={handleSubmit}>
 					<XSvg className='w-24 lg:hidden fill-white' />
 					<h1 className='text-4xl font-extrabold text-white'>{"Let's"} go.</h1>
 					<label className='input input-bordered rounded flex items-center gap-2'>
